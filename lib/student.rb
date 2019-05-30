f3881a7b88d4c16da958dbc456f83a69e3822f73
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-05-30 04:31:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,7 @@ class Student
       DROP TABLE students
     SQL
 
-    DB[:conn](sql)
+    DB[:conn].execute(sql)
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
